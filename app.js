ba94392f11850d537b2566d4148b330f86e536b3
ali hasan <alihassanraja06@gmail.com>
2024-08-12 10:48:51 +0500

    updated

--- app.js
+++ app.js
@@ -1,50 +1,45 @@
-const title = document.querySelector('#title')
-const description = document.querySelector('#description')
-const price = document.querySelector('#Price')
-const div = document.querySelector('#div')
-const btn = document.querySelector('#button')
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+const price = document.querySelector('#Price');
+const div = document.querySelector('#div');
+const btn = document.querySelector('#button');
 
-let arr = [];
+let arr = JSON.parse(localStorage.getItem('arr')) || [];
 
-function AddObject(){
-    // console.log('adjbj');
-let obj ={
-    title :title.value,
-    description: description.value,
-    price:price.value
+function AddObject() {
+    let obj = {
+        title: title.value,
+        description: description.value,
+        price: price.value
+    };
 
+    arr.push(obj);
+
+    localStorage.setItem('arr', JSON.stringify(arr));
+
+    title.value = '';
+    description.value = '';
+    price.value = '';
+
+    renderCards();
 }
-arr.push(obj);
 
-arr.map((item)=>{
-
-div.innerHTML +=`
-
-
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Title :${item.title}</h5>
-      <p class="card-text">Description :${item.description}</p>
-      <p class="card-text">Price :${item.price}</p>
-      <button class="btn btn-primary">Add to cart</button>
-    </div>
-  </div>
-
-
-
-
-`
-
-title.value =''
-description.value =''
-price.value =''
-
-console.log(item);
-
-
-let arrstring = JSON.stringify(arr)
-
-localStorage.setItem('arr',cartItems)
-})
-    
+function renderCards() {
+    div.innerHTML = '';
+    arr.forEach(item => {
+        div.innerHTML += `
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Title : ${item.title}</h5>
+                  <p class="card-text">Description : ${item.description}</p>
+                  <p class="card-text">Price : ${item.price}</p>
+                  <button class="btn btn-primary">Add to cart</button>
+                </div>
+            </div>
+        `;
+    });
 }
+
+renderCards();
+
+btn.addEventListener('click', AddObject);
